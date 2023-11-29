520f022d52aa1563d34500583599a69c8274827a
ermias30 <eyntlmi@yahoo.com>
2023-11-29 13:56:03 +0300

    add more pages

--- app_3.py
+++ app_3.py
@@ -5,11 +5,15 @@ import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 df=pd.read_csv("topics.csv")
 
+
 for index,row in df.iterrows():
     pdf.add_page()
+    # print(row["ermi"])
     pdf.set_font(family="Times",size=12,style="B")
     pdf.cell(w=0,h=12,txt=row["Topic"],align="l",ln=1,border=0)
     pdf.line(10,22,200,22)
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
 
 
 
